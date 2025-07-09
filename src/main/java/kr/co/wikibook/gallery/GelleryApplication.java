3ece0aaa9b04e88ec014aa9020569cc973de6101
supii-dev <hwangsubin93@gmail.com>
2025-07-09 18:00:51 +0900

    1

---
+++ src/main/java/kr/co/wikibook/gallery/GelleryApplication.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GelleryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/wikibook/gallery/GelleryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
